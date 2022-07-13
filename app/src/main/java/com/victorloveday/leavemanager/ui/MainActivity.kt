41230511e0ee91a742f2d9a326d54a220e9e522b
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-07-13 19:25:25 +0100

    Feat: set up nav host fragment

--- app/src/main/java/com/victorloveday/leavemanager/ui/MainActivity.kt
+++ app/src/main/java/com/victorloveday/leavemanager/ui/MainActivity.kt
@@ -2,18 +2,41 @@ package com.victorloveday.leavemanager.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.victorloveday.leavemanager.R
 import com.victorloveday.leavemanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //set bottom nav color to invisible && disable middle icon on bottom nav
         binding.bottomNavigationView.background = null
         binding.bottomNavigationView.menu.getItem(1).isEnabled = false
+        binding.bottomNavigationView.itemIconTintList = null
+
+
+        // setup bottom nav
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        binding.bottomNavigationView.setOnItemReselectedListener {
+            return@setOnItemReselectedListener
+        }
+
+        //feeds icon badge badge
+        val badge = binding.bottomNavigationView.getOrCreateBadge(R.id.homeFragment)
+        badge.isVisible = true
     }
 }
