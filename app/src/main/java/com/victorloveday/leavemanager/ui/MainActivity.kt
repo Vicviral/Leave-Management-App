3ab1b400375edf8f28eea8d48b827538ce2d6685
Victor Loveday <56683410+Vicviral@users.noreply.github.com>
2022-07-13 23:12:28 +0100

    Feat: set up tool bar with nav controller

--- app/src/main/java/com/victorloveday/leavemanager/ui/MainActivity.kt
+++ app/src/main/java/com/victorloveday/leavemanager/ui/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.victorloveday.leavemanager.R
 import com.victorloveday.leavemanager.databinding.ActivityMainBinding
@@ -13,23 +15,27 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var appBarConfig: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //set bottom nav color to invisible && disable middle icon on bottom nav
-        binding.bottomNavigationView.background = null
-        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
-        binding.bottomNavigationView.itemIconTintList = null
-
-
-        // setup bottom nav
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        //setup nav host
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
+        //setup app bar configuration
+        appBarConfig = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.historyFragment, R.id.notificationFragment, R.id.profileFragment)
+        )
+
+        //setup toolbar
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfig)
+
+        // setup bottom nav
         binding.bottomNavigationView.setupWithNavController(navController)
         binding.bottomNavigationView.setOnItemReselectedListener {
             return@setOnItemReselectedListener
@@ -38,5 +44,14 @@ class MainActivity : AppCompatActivity() {
         //feeds icon badge badge
         val badge = binding.bottomNavigationView.getOrCreateBadge(R.id.homeFragment)
         badge.isVisible = true
+
+        //set bottom nav color to invisible && disable middle icon on bottom nav
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+        binding.bottomNavigationView.itemIconTintList = null
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
